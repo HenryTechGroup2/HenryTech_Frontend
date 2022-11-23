cccec05bb78fac84cc18399e3da5e796c932846a
unknown <lauravalenciapolo@gmail.com>
2022-11-23 16:05:17 -0500

    Componente Questions y About us del footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Questions from './pages/Questions.js';
+import Aboutus from "./pages/Aboutus.js"
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/home'/>
+        <Route path='/preguntasfrecuentes' element={<Questions/>} />
+        <Route path='/sobrenosotros' element={<Aboutus/>} />
       </Routes>
     </BrowserRouter>
   );
