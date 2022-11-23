2e92dd6f39cef35c210d0fb2e1c13bdc9ef93ddd
Davidcg182 <davidcasti182@gmail.com>
2022-11-23 18:55:41 -0500

    SearchBar and Filter

--- src/redux/reducer/reducerFetch.js
+++ src/redux/reducer/reducerFetch.js
@@ -1,8 +1,15 @@
 
+<<<<<<< HEAD
 const initialState = {
   products: [],
   userloggin: false,
   stockProducts: [{ title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", stock: 10 }, { title: "Mens Casual Premium Slim Fit T-Shirts ", stock: 35 }, { title: "Mens Cotton Jacket", stock: 4 }]
+=======
+
+const inicialState = {
+  copieProducts: [],
+  products: []
+>>>>>>> afa496f (SearchBar and Filter)
 }
 
 export const reducerFetch = (state = initialState, action) => {
@@ -23,7 +30,21 @@ export const reducerFetch = (state = initialState, action) => {
       ...state,
       products: action.payload
     }
+<<<<<<< HEAD
     default:
       return state;
+=======
+
+    case 'FILTER_BY_PRICE': {
+      let filterproducts = state.copieProducts.filter(e => e.product_price <= action.payload)
+      return {
+        ...state,
+        products: filterproducts
+      }
+    }
+
+    default: return { ...state }
+>>>>>>> afa496f (SearchBar and Filter)
   }
 };
+
