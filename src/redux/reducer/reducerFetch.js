c6ddc0fc16fd0368f51cac305e7a8595864401bd
unknown <lauravalenciapolo@gmail.com>
2022-11-23 13:27:58 -0500

    Card con validación del stock

--- src/redux/reducer/reducerFetch.js
+++ src/redux/reducer/reducerFetch.js
@@ -1,6 +1,7 @@
 const initialState = {
   products:[],
   userloggin: false,
+  stockProducts: [{title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops", stock:10},{title:"Mens Casual Premium Slim Fit T-Shirts ", stock:35},{title:"Mens Cotton Jacket", stock:4}]
 
 }
 
@@ -10,6 +11,12 @@ export const reducerFetch = (state= initialState, action) => {
       return {
         ...state,
         products: action.payload
+      }
+    }
+    case "GET_STOCK_PRODUCTS":{
+      return {
+        ...state,
+        stockProducts: action.payload
       }
     }
     
