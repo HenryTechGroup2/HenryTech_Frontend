d8854f1ec2aaf9260bbcef6052b0a9907578e9a1
Jonatan <jonatanangel88@gmail.com>
2022-11-25 12:37:34 -0300

    background

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 const useUser = () => {
   const { userDates } = useSelector((state) => state);
   const navigate = useNavigate();
-
+  console.log(userDates);
   useEffect(() => {
     if (userDates.hasOwnProperty('user_email')) {
     } else {
